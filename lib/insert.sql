34e15a7d7466cebb57b453b9597f2a05e062c019
Andrew <atg27@uw.edu>
2021-05-26 20:02:20 -0700

    commit tree fd5e6d18650d3a342ca4801df3295ee2247fb004

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "black", "mad", true);
+INSERT INTO bears VALUES  (2, "Rowdy", 1, "M", "black", "rowdy", true);
+INSERT INTO bears VALUES (3,"Tabitha", 12, "F", "brown", "mad", true);
+INSERT INTO bears VALUES  (4,"Sergeant Brown", 1, "M", "black", "mad", true);
+INSERT INTO bears VALUES  (5,"Melissa", 4, "F", "black", "happy", true);
+INSERT INTO bears VALUES (6,"Grinch", 1, "M", "black", "mad", true);
+INSERT INTO bears VALUES  (7,"Wendy", 1, "M", "black", "mad", true);
+INSERT INTO bears VALUES (8,null, 1, "M", "brown", "violent", true);
